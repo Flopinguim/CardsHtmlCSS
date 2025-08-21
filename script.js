c5c2abbde97fded30c2ef88de52e0726b5aa80ef
Flopinguim <08joaovitor01@gmail.com>
2025-08-21 10:51:31 -0300

    modal pokemon api

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
     document.getElementById('year').textContent = new Date().getFullYear();
+
+    async function pokemonDetailsClick(button) {
+        var card = button.closest('.card');
+        var pokemonName = card.querySelector('.card-title').textContent;
+        var response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`);
+        var pokemon = await response.json();
+
+        Swal.fire({
+            title: `${pokemon.id} - ${pokemon.name}`,
+            html: `
+            <div class="pokemon-picture">
+                <img src="${pokemon.sprites.front_default}"  />
+                <div class="pokemon-info">
+                    <h3 class="type">Type: ${pokemon.types.map(item => item.type.name).toString()}</p>
+                    <h3 class="weight">Weight: ${pokemon.weight  / 10}kg</p>
+                    <h3 class="height">Height: ${pokemon.height  / 10}m</p>
+                </div>        
+            </div>`
+          });
+    }
